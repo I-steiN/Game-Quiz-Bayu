395d199513be187892ea8c39ea96a91a66d27898
Ilham Ramadhan Tamba <57670670+I-steiN@users.noreply.github.com>
2019-11-21 03:49:44 +0700

    Add files via upload

---
+++ Assets/Script/Gameplay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gameplay : MonoBehaviour
+{
+    public string [] Question, Answer;
+    public Text text_Question, text_Score;
+    public InputField input_Answer;
+    public GameObject Feed_Right, Feed_False, Done, Question_Spot;
+    int urutan_soal = -1, Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tampil_soal ();   
+    }
+
+    void tampil_soal ()
+    {
+        urutan_soal ++;
+        text_Question.text = Question [urutan_soal];
+    }
+
+    public void answer () 
+    {
+        if (urutan_soal < Question.Length - 1)
+        {
+        if (input_Answer.text == Answer [urutan_soal])
+        {
+            Feed_Right.SetActive (false);
+            Feed_Right.SetActive (true);
+            Feed_False.SetActive (false);
+            Score += 20;
+    } else {
+            Feed_Right.SetActive (false);
+            Feed_False.SetActive (false);
+            Feed_False.SetActive (true);
+            }
+            input_Answer.text = "";
+            tampil_soal ();
+        } else {
+          if (input_Answer.text == Answer [urutan_soal])
+          {
+              Score += 20;
+          }  
+            Done.SetActive (true);
+            Question_Spot.SetActive (false);
+        }  
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        text_Score.text = Score.ToString ();  
+    }
+}
